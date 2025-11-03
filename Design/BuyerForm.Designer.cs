35f68031c7b2c841a98c8b47f1859175314e706c
ryhan27 <muhamadrayhanramadhansyah80@gmail.com>
2025-11-03 13:54:42 +0700

    rayhan

--- Design/BuyerForm.Designer.cs
+++ Design/BuyerForm.Designer.cs
@@ -48,178 +48,174 @@
             label7 = new Label();
             label8 = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvBuyer).BeginInit();
-            //SuspendLayout();
+            SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(49, 84);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(39, 67);
             label1.Name = "label1";
-            label1.Size = new Size(117, 28);
+            label1.Size = new Size(99, 23);
             label1.TabIndex = 0;
             label1.Text = "Buyer Form";
-            //label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(49, 130);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(39, 104);
             label2.Name = "label2";
-            label2.Size = new Size(103, 25);
+            label2.Size = new Size(85, 20);
             label2.TabIndex = 1;
             label2.Text = "Buyer Code";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(49, 177);
-            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Location = new Point(39, 142);
             label3.Name = "label3";
-            label3.Size = new Size(108, 25);
+            label3.Size = new Size(90, 20);
             label3.TabIndex = 2;
             label3.Text = "Buyer Name";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(49, 225);
-            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Location = new Point(39, 180);
             label4.Name = "label4";
-            label4.Size = new Size(59, 25);
+            label4.Size = new Size(50, 20);
             label4.TabIndex = 3;
             label4.Text = "Email ";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(49, 276);
-            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Location = new Point(39, 221);
             label5.Name = "label5";
-            label5.Size = new Size(132, 25);
+            label5.Size = new Size(108, 20);
             label5.TabIndex = 4;
             label5.Text = "Phone Number";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(49, 335);
-            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Location = new Point(39, 268);
             label6.Name = "label6";
-            label6.Size = new Size(77, 25);
+            label6.Size = new Size(62, 20);
             label6.TabIndex = 5;
             label6.Text = "Address";
             // 
             // txtCode
             // 
-            txtCode.Location = new Point(208, 124);
-            txtCode.Margin = new Padding(4, 4, 4, 4);
+            txtCode.Location = new Point(166, 99);
             txtCode.Name = "txtCode";
-            txtCode.Size = new Size(314, 31);
+            txtCode.Size = new Size(252, 27);
             txtCode.TabIndex = 6;
             // 
             // txtName
             // 
-            txtName.Location = new Point(208, 174);
-            txtName.Margin = new Padding(4, 4, 4, 4);
+            txtName.Location = new Point(166, 139);
             txtName.Name = "txtName";
-            txtName.Size = new Size(314, 31);
+            txtName.Size = new Size(252, 27);
             txtName.TabIndex = 7;
             // 
             // txtEmail
             // 
-            txtEmail.Location = new Point(208, 219);
-            txtEmail.Margin = new Padding(4, 4, 4, 4);
+            txtEmail.Location = new Point(166, 175);
             txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(314, 31);
+            txtEmail.Size = new Size(252, 27);
             txtEmail.TabIndex = 8;
             // 
             // txtPhone
             // 
-            txtPhone.Location = new Point(208, 276);
-            txtPhone.Margin = new Padding(4, 4, 4, 4);
+            txtPhone.Location = new Point(166, 221);
             txtPhone.Name = "txtPhone";
-            txtPhone.Size = new Size(314, 31);
+            txtPhone.Size = new Size(252, 27);
             txtPhone.TabIndex = 9;
             // 
             // txtAddress
             // 
-            txtAddress.Location = new Point(208, 329);
-            txtAddress.Margin = new Padding(4, 4, 4, 4);
+            txtAddress.Location = new Point(166, 263);
             txtAddress.Name = "txtAddress";
-            txtAddress.Size = new Size(314, 31);
+            txtAddress.Size = new Size(252, 27);
             txtAddress.TabIndex = 10;
             // 
             // btnAddBuyer
             // 
-            btnAddBuyer.Location = new Point(49, 396);
-            btnAddBuyer.Margin = new Padding(4, 4, 4, 4);
+            btnAddBuyer.BackColor = Color.FromArgb(45, 45, 45);
+            btnAddBuyer.FlatAppearance.BorderSize = 0;
+            btnAddBuyer.FlatStyle = FlatStyle.Flat;
+            btnAddBuyer.Location = new Point(39, 317);
             btnAddBuyer.Name = "btnAddBuyer";
-            btnAddBuyer.Size = new Size(118, 36);
+            btnAddBuyer.Size = new Size(94, 29);
             btnAddBuyer.TabIndex = 11;
             btnAddBuyer.Text = "Add";
-            btnAddBuyer.UseVisualStyleBackColor = true;
+            btnAddBuyer.UseVisualStyleBackColor = false;
             // 
             // btnUpdate
             // 
-            btnUpdate.Location = new Point(174, 396);
-            btnUpdate.Margin = new Padding(4, 4, 4, 4);
+            btnUpdate.BackColor = Color.FromArgb(45, 45, 45);
+            btnUpdate.FlatAppearance.BorderSize = 0;
+            btnUpdate.FlatStyle = FlatStyle.Flat;
+            btnUpdate.Location = new Point(139, 317);
             btnUpdate.Name = "btnUpdate";
-            btnUpdate.Size = new Size(118, 36);
+            btnUpdate.Size = new Size(94, 29);
             btnUpdate.TabIndex = 12;
             btnUpdate.Text = "Update";
-            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.UseVisualStyleBackColor = false;
             // 
             // btnDelete
             // 
-            btnDelete.Location = new Point(299, 396);
-            btnDelete.Margin = new Padding(4, 4, 4, 4);
+            btnDelete.BackColor = Color.FromArgb(45, 45, 45);
+            btnDelete.FlatAppearance.BorderSize = 0;
+            btnDelete.FlatStyle = FlatStyle.Flat;
+            btnDelete.Location = new Point(239, 317);
             btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(118, 36);
+            btnDelete.Size = new Size(94, 29);
             btnDelete.TabIndex = 13;
             btnDelete.Text = "Delete";
-            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.UseVisualStyleBackColor = false;
             // 
             // btnClear
             // 
-            btnClear.Location = new Point(424, 396);
-            btnClear.Margin = new Padding(4, 4, 4, 4);
+            btnClear.BackColor = Color.FromArgb(45, 45, 45);
+            btnClear.FlatAppearance.BorderSize = 0;
+            btnClear.FlatStyle = FlatStyle.Flat;
+            btnClear.Location = new Point(339, 317);
             btnClear.Name = "btnClear";
-            btnClear.Size = new Size(118, 36);
+            btnClear.Size = new Size(94, 29);
             btnClear.TabIndex = 14;
             btnClear.Text = "Clear";
-            btnClear.UseVisualStyleBackColor = true;
+            btnClear.UseVisualStyleBackColor = false;
             // 
             // dgvBuyer
             // 
             dgvBuyer.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvBuyer.Location = new Point(552, 130);
-            dgvBuyer.Margin = new Padding(4, 4, 4, 4);
+            dgvBuyer.Location = new Point(442, 104);
             dgvBuyer.Name = "dgvBuyer";
             dgvBuyer.RowHeadersWidth = 51;
-            dgvBuyer.Size = new Size(513, 321);
+            dgvBuyer.Size = new Size(410, 257);
             dgvBuyer.TabIndex = 15;
             // 
             // btnBack
             // 
-            btnBack.Location = new Point(49, 477);
-            btnBack.Margin = new Padding(4);
+            btnBack.BackColor = Color.FromArgb(45, 45, 45);
+            btnBack.FlatAppearance.BorderSize = 0;
+            btnBack.FlatStyle = FlatStyle.Flat;
+            btnBack.Location = new Point(39, 382);
             btnBack.Name = "btnBack";
-            btnBack.Size = new Size(118, 36);
+            btnBack.Size = new Size(94, 29);
             btnBack.TabIndex = 16;
             btnBack.Text = "Back";
-            btnBack.UseVisualStyleBackColor = true;
+            btnBack.UseVisualStyleBackColor = false;
             // 
             // label7
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label7.Location = new Point(552, 84);
-            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Location = new Point(442, 67);
             label7.Name = "label7";
-            label7.Size = new Size(111, 28);
+            label7.Size = new Size(95, 23);
             label7.TabIndex = 17;
             label7.Text = "Buyer Data";
             // 
@@ -227,17 +223,19 @@
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label8.Location = new Point(359, 27);
+            label8.Location = new Point(287, 22);
+            label8.Margin = new Padding(2, 0, 2, 0);
             label8.Name = "label8";
-            label8.Size = new Size(316, 38);
+            label8.Size = new Size(272, 32);
             label8.TabIndex = 18;
             label8.Text = "🏦  SBN APPLICATION";
             // 
             // BuyerForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1078, 544);
+            BackColor = Color.FromArgb(30, 30, 30);
+            ClientSize = new Size(862, 435);
             Controls.Add(label8);
             Controls.Add(label7);
             Controls.Add(btnBack);
@@ -257,7 +255,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(4, 4, 4, 4);
+            ForeColor = Color.White;
             Name = "BuyerForm";
             Text = "BuyerForm";
             Load += BuyerForm_Load;
