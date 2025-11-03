497b7a4ddcff5ce3e4ade2e9309a0c6e1f8fd73e
DevTria <devtria249@gmail.com>
2025-11-03 13:52:03 +0700

    Satria

---
+++ Service/AssetService.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using SbnApplicationUTS.Data;
+using SbnApplicationUTS.Model;
+
+namespace SbnApplicationUTS.Services
+{
+    public class AssetService
+    {
+        private readonly AppDbContext _db;
+        public AssetService(AppDbContext db) => _db = db;
+        public List<object> SetDropdown()
+        {
+            var list = _db.Assets.OrderBy(m => m.Nama_Asset)
+                .Select(m => new
+                {
+                    m.Id,
+                    DisplayName = m.Id + " - " + m.Nama_Asset
+                })
+                .ToList<object>();
+            return list;
+        }
+
+        public Asset? FindById(int id)
+        {
+            return _db.SBNs.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<Asset> SetGrid()
+        {
+            return
+                _db.SBNs.OrderByDescending(m => m.ModDate).ToList<Asset>();
+        }
+
+        public async void Update(Asset Asset)
+        {
+            _db.SBNs.Update(Asset);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
